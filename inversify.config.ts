288c7cc27dc6878b186d7c94ceab0618d9ee0094
Alex Bondo Andersen <abondoa@gmail.com>
2020-02-13 23:16:38 +0100

    Init

---
+++ inversify.config.ts
@@ -0,0 +1,16 @@
+import { Container } from 'inversify';
+import { ApplicationEnvironmentRepository, Coder, StreamFactory } from 'gq';
+import { FileStreamFactory } from 'gq';
+import { JsonCoder } from 'gq';
+import { TYPES } from 'gq';
+
+export function createContainer(applicationEnvironmentRoot: string): Container {
+    const container = new Container();
+    container.bind<ApplicationEnvironmentRepository>(ApplicationEnvironmentRepository)
+        .toSelf().inSingletonScope();
+    container.bind<StreamFactory>(TYPES.StreamFactory).to(FileStreamFactory);
+    container.bind<Coder>(TYPES.Coder).to(JsonCoder);
+    container.bind<string>(TYPES.ApplicationEnvironmentRoot)
+        .toConstantValue(applicationEnvironmentRoot);
+    return container;
+}
